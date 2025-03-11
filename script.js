c6d7e2d1e9baeb4f606ed287fdf490091c1a68ee
JingyeDev <jayzhangm12@gmail.com>
2025-03-11 16:07:31 +0800

    Change the user input from prompt to button

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["✊", "✋", "✌️"];
 let humanScore = 0;
 let computerScore = 0;
-
+let round = 1;
 function getComputerChoice() {
   const rand = Math.floor(Math.random() * 3);
   return choices[rand];
 }
 
-function getHumanChoice() {
-  let humanChoice;
-  do {
-    humanChoice = prompt("Enter your choice...");
-    if (!choices.includes(humanChoice)) {
-      console.log("Invalid input! Try again.");
-    }
-  } while (!choices.includes(humanChoice));
-  return humanChoice.toLowerCase();
-}
-
 function playRound(humanSelection, computerSelection) {
+  h1.innerText = "Rock Paper Scissors game";
+  h1.style.backgroundColor = "";
+  h1.style.fontSize = "";
+  display.innerText = `Round#${round}`
   if (humanSelection === computerSelection) {
-    console.log("It's a draw!");
+    display.innerText += "\nDraw!"
   } else if (
-    (humanSelection === "rock" && computerSelection === "scissors") ||
-    (humanSelection === "paper" && computerSelection === "rock") ||
-    (humanSelection === "scissors" && computerSelection === "paper")
+    (humanSelection === "✊" && computerSelection === "✌️") ||
+    (humanSelection === "✋" && computerSelection === "✊") ||
+    (humanSelection === "✌️" && computerSelection === "✋")
   ) {
     humanScore++;
-    console.log(`${humanSelection} beats ${computerSelection} - You win!`);
+   display.innerText += `\n${humanSelection} beats ${computerSelection} - You win!`;
   } else {
     computerScore++;
-    console.log(
-      `${computerSelection} beats ${humanSelection} - Computer wins!`
-    );
+    display.innerText += `\n${computerSelection} beats ${humanSelection} - Computer wins!`;
   }
+  display.innerText += `\nYou score:${humanScore}, Computer Scores: ${computerScore}`;
+
+  if (humanScore >= 5 || computerScore >= 5) {
+    if (humanScore > computerScore) {
+      h1.innerText = "\nCongratulations, you won!" 
+    } else if (computerScore > humanScore) {
+      h1.innerText = "\nComputer won!"
+    } else {
+    h1.innerText = "\nDraw!" 
+  }
+  h1.style.backgroundColor = "tomato";
+  h1.style.fontSize = "2rem";
+  humanScore = 0;
+  computerScore = 0;
+  round = 1;
+}
+  round++;
 }
 
-function playGame() {
-  let round = 1;
-  while (round <= 5) {
-    console.log(`Round #${round}`);
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-    round++;
-  }
+const buttons = document.querySelector("#buttons");
+let display = document.querySelector("#display");
+const h1 = document.querySelector("h1");
 
-  console.log("\nFinal Score:");
-  console.log(`You: ${humanScore} | Computer: ${computerScore}`);
-  if (humanScore > computerScore) {
-    console.log("Congratulations, You won!");
-  } else if (humanScore < computerScore) {
-    console.log("Computer won.");
-  } else {
-    console.log("It's a draw!");
-  }
-}
+buttons.addEventListener('click', e => {
+  const humanChoice = e.target.innerText;
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+})
 
-playGame();
+
+
